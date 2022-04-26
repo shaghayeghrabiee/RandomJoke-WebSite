e3d763fef9eda2384d4e688b12381d1f924b25de
shaghayegh rabiee <rabiee.sh65@gmail.com>
2022-04-26 17:24:38 +0200

    pagination

--- src/pages/queryPage.jsx
+++ src/pages/queryPage.jsx
@@ -1,36 +1,41 @@
 import React, { useState } from "react";
 import { searchQuery } from "../services/apiCalls";
 import ReactPaginate from 'react-paginate';
-import "./queryPage.css";
+import style from "./queryPage.module.css";
 
 const QueryPage = () => {
-  const [searchTerm, setSearchTerm] = useState(0);
+  const [searchTerm, setSearchTerm] = useState();
   const [pageNumber,setPageNumber]=useState(0);
 
-  const JokePerPage=10;
-  const visitedPage= (pageNumber*JokePerPage);
+  const queryPerPage=8;
+  const visitedpages= pageNumber * queryPerPage;
+  const pageCount= Math.ceil(searchTerm && searchTerm.length /queryPerPage);
   console.log(searchTerm);
-  const pageCount= searchTerm ?  Math.ceil(searchTerm.length/JokePerPage):0;
-
-  const displayJoke=  searchTerm && searchTerm.slice(visitedPage, JokePerPage+visitedPage).map((query, index) =>
-        {return (    <li key={index}>
-          <span>
-            {" "}
-            <strong> Category:</strong>{" "}
-          </span>
-          {query.categories.length ? (
-            <span className="hasCategory">{query.categories}</span>
-          ) : (
-            <span className="noCategory">Does not mention </span>
-          )}
-          <br />
-          <br/>
-          <span>
-            <strong> Joke: </strong>
-          </span>
-          {query.value}
-        </li>)})
-
+  const displayJoke= 
+      searchTerm && searchTerm
+            .slice(visitedpages, visitedpages+queryPerPage)
+                .map((query, index)=>{return (  
+                  <ul>
+                    <li key={index}>
+                      <span>
+                        {" "}
+                        <strong> Category:</strong>{" "}
+                      </span>
+                      {query.categories.length ? (
+                      <span className={style.hasCategory}>{query.categories}</span>
+                      ) : (
+                        <span className={style.noCategory}>Does not mention </span>
+                      )}
+                      <br />
+                      <br/>
+                      <span>
+                        <strong> Joke: </strong>
+                      </span>
+                      { query.value }
+                    </li>
+                  </ul>  
+                )}
+              )
 
   const showQuery = (e) => {
     e.preventDefault();
@@ -40,60 +45,45 @@ const QueryPage = () => {
       setSearchTerm(res.data.result);
     });
   };
-const handlePage=({selected})=>{
-  setPageNumber(selected)
-}
-  return (
-    <div className="queryContainer">
-      <div class="flexbox">
-        <div class="search">
-          <h1>Search your random joke</h1>
-          <h3>Click on search icon, then type your keyword.</h3>
-          <div>
-            <input
-              type="text"
-              placeholder="Search . . ."
-              onChange={showQuery}
-              required
-            />
-          </div>
-        </div>
-      </div>
 
-      <ul>
-      {displayJoke}
-      <ReactPaginate
-       previousLabel={"previous"}
-       nextLabel={"next"}
-       pageCount={"pageCount"}
-       onPageChange={handlePage}
-       containerClassName={"paginationBttns"}
-       previousLinkClassName={"previousBttns"}
-       nextLinkClassName={"nextBttns"}
-       disabledClassName={"paginationDisabled"}
-       activeClassName={"paginationActive"}/>
-      </ul>
+  const handlePage=({selected})=>{
+    setPageNumber(selected)
+  }
+
+  return (
+    <div>
+          <div className={style.queryContainer}>
+          <div className={style.flexbox}>
+            <div className={style.search}>
+              <h1>Search your random joke</h1>
+              <h3>Click on search icon, then type your keyword.</h3>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Search . . ."
+                  onChange={showQuery}
+                  required
+                />
+              </div>
+            </div>
+          </div>
+            <div className={style.result}>
+            {searchTerm && searchTerm.length !==0 ? displayJoke : "Nothing has Found"}
+            </div>
+            
+            {/* {displayJoke} */}
+          </div>
+          <ReactPaginate 
+          previousLabel={"Previous"}
+          nextLabel={"Next"}
+          pageCount={pageCount}
+          onPageChange={handlePage}
+          containerClassName={style.paginationBttns}
+          previousLinkClassName={style.previousBttns}
+          nextLinkClassName={style.nextBttns}
+          disabledClassName={style.paginationDisabled}
+          activeClassName={style.paginationActive}/>
     </div>
   );
 };
 export default QueryPage;
-// searchTerm &&
-// searchTerm.map((query, index) => (
-//   <li key={index}>
-//     <span>
-//       {" "}
-//       <strong> Category:</strong>{" "}
-//     </span>
-//     {query.categories.length ? (
-//       <span className="hasCategory">{query.categories}</span>
-//     ) : (
-//       <span className="noCategory">Does not mention </span>
-//     )}
-//     <br />
-//     <br/>
-//     <span>
-//       <strong> Joke: </strong>
-//     </span>
-//     {query.value}
-//   </li>
-// ))
